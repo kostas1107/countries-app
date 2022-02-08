b02f9d8689c65079860716667b823adb2b0d6f4e
Kostas Sudzius <kostas1107@gmail.com>
2022-02-08 13:53:38 +0200

    installed fontawesome

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
